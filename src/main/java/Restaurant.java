6d266e6fa7c6f15778666828bc0dcc0343f239af
ambrish510 <ambrishpatil510@gmail.com>
2021-05-03 04:35:41 +0530

    Second Commit

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -3,7 +3,6 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
-//Restauramt POJO
 public class Restaurant {
     private String name;
     private String location;
