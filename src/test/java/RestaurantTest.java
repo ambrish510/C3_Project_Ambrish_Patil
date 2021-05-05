e098c21131985bce2e29b16a75706d0587eab31d
patil ambrish <33837950+ambrish510@users.noreply.github.com>
2021-05-05 13:09:31 +0530

    Added Test Cases for getMenu Method in restaurant class

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -11,7 +11,7 @@ class RestaurantTest{
     @Test
     public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
         openingTime = LocalTime.parse("10:30:00");
-        closingTime = LocalTime.parse("11:00:00");
+        closingTime = LocalTime.parse("22:00:00");
         restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
         System.out.println(restaurant.isRestaurantOpen());
         assertTrue(restaurant.isRestaurantOpen());
@@ -20,49 +20,11 @@ class RestaurantTest{
     @Test
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
         openingTime = LocalTime.parse("10:30:00");
-        closingTime = LocalTime.parse("18:00:00");
+        closingTime = LocalTime.parse("12:00:00");
         restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
         assertFalse(restaurant.isRestaurantOpen());
     }
-
-    @Test
-    public void get_menu_should_return_all_items_in_menu(){
-        openingTime = LocalTime.parse("10:30:00");
-        closingTime = LocalTime.parse("22:00:00");
-        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-        assertNotNull(restaurant.getMenu());
-    }
-
-    @Test
-    public void searching_for_existing_item_in_menu_should_return_expected_item(){
-        openingTime = LocalTime.parse("10:30:00");
-        closingTime = LocalTime.parse("22:00:00");
-        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-        assertNotNull(restaurant.findItemByName("Vegetable lasagne"));
-    }
-
-    @Test
-    public void searching_for_non_existing_item_in_menu_should_return_null_item(){
-        openingTime = LocalTime.parse("10:30:00");
-        closingTime = LocalTime.parse("22:00:00");
-        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        restaurant.addToMenu("Sweet corn soup",119);
-        restaurant.addToMenu("Vegetable lasagne", 269);
-        assertNull(restaurant.findItemByName("Vegetable"));
-    }
-
-    @Test
-    public void get_Current_Time_should_return_current_time(){
-        openingTime = LocalTime.parse("10:30:00");
-        closingTime = LocalTime.parse("22:00:00");
-        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
-        assertEquals(LocalTime.now(),restaurant.getCurrentTime());
-    }
-
+    
     @Test
     public void adding_item_to_menu_should_increase_menu_size_by_1(){
         openingTime = LocalTime.parse("10:30:00");
@@ -96,5 +58,23 @@ class RestaurantTest{
         restaurant.addToMenu("Vegetable lasagne", 269);
         assertThrows(itemNotFoundException.class,
                 ()->restaurant.removeFromMenu("French fries"));
+    }
+
+    @Test
+    public void get_menu_should_return_all_items_in_menu(){
+        openingTime = LocalTime.parse("10:30:00");
+        closingTime = LocalTime.parse("22:00:00");
+        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        assertNotNull(restaurant.getMenu());
+    }
+
+    @Test
+    public void get_menu_should_return_null_object_if_no_item_found(){
+        openingTime = LocalTime.parse("10:30:00");
+        closingTime = LocalTime.parse("22:00:00");
+        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        assertNotNull(restaurant.getMenu());
     }
 }
